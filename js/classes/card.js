04a481bd430280c2b3f1d0ced5f3863b22fb984c
Horskyj Oleh <ogorski23@gmail.com>
2020-06-06 21:30:05 +0000

    Replace card.js | Need fix

--- js/classes/card.js
+++ js/classes/card.js
@@ -1,4 +1,16 @@
 export default class Card {
-  constructor() {
-  }
+    constructor(body, info) {
+        this._body = body;
+        this._info = info;
+        this.elem = null;
+    }
+
+
+    render() {
+        this._info = document.createElement('div');
+        this._info.classList.add('card-info');
+        this._body = document.createElement('div');
+        this._body.classList.add('card-body');
+        this.elem.append(this._body);
+    }
 }
